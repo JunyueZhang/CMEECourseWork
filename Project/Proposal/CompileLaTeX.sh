93acedb31113285ccf6e25533d96c46e0dc582a8
Junyue Zhang <jz1621@ic.ac.uk>
2022-03-31 21:29:52 +0100

    Upload proposal

---
+++ Project/Proposal/CompileLaTeX.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+name=$(echo "$1" | cut -f 1 -d '.')
+pdflatex $name.tex
+bibtex $name
+pdflatex $name.tex
+pdflatex $name.tex
+evince $name.pdf &
+
+## Cleanup
+rm *.aux
+rm *.log
+rm *.bbl
+rm *.blg
